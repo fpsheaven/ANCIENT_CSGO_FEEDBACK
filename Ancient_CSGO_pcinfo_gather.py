acc87ef2f1befc9672fbd8026fd04df0d1ebc7ac
frequencycs <58235292+fpsheaven@users.noreply.github.com>
2022-11-24 00:36:13 +0200

    Update Ancient_CSGO_pcinfo_gather.py
    Fixed typos

--- Ancient_CSGO_pcinfo_gather.py
+++ Ancient_CSGO_pcinfo_gather.py
@@ -9,7 +9,7 @@ print ("run this program only if you are having issues on ancient.only!")
 print("The program will work for 3 days (trial ends),need to find a ftp server to upload the files.")
 print ("It will export the system specs to a folder on the c drive.")
 print ("are you sure you are having issues ONLY on ANCIENT?")
-quit=input("If you do, press Y and ENTER or if you, dont press N and ENTER to exit the utility")
+quit=input("If you do, press Y or if you, dont press N to exit the utility   ")
 if quit=="N" or quit=="n":
     sys.exit()
 else:
@@ -22,14 +22,11 @@ user_file=os.listdir(path)[0] #OUTPUT NAME
 
 ssh=paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
-ssh.connect(hostname="IPHIDDEN",username="server",password="PASSWORD",port=22)
+ssh.connect(hostname="64.227.38.17",username="server",password="Serverpassword1",port=22)
 
 sftp_client=ssh.open_sftp()
 sftp_client.put("C:/frequencycs/"+user_file,"/ancient/"+user_file)
 sftp_client.close()
 ssh.close()
 print("File uploaded")
-a = input("For FPS optimizations DM me on twitter! Press any key to exit")
-if a:
-    webbrowser.open_new_tab("https://twitter.com/fREQUENCYCS")
-    exit(0)
+exit(0)
