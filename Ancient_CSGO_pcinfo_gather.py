5a8efe4a9248d4e7258c814eddc82b91b14c40b7
frequencycs <58235292+fpsheaven@users.noreply.github.com>
2022-11-23 23:37:26 +0200

    Add files via upload

---
+++ Ancient_CSGO_pcinfo_gather.py
@@ -0,0 +1,35 @@
+import os,sys,datetime,paramiko,shutil,webbrowser
+path="C:\\frequencycs"
+if os.path.isdir(path)==1:
+    shutil.rmtree(path)
+
+date=str(datetime.date.today())
+os.mkdir("C:\\frequencycs");os.chdir("C:\\frequencycs");
+print ("run this program only if you are having issues on ancient.only!")
+print("The program will work for 3 days (trial ends),need to find a ftp server to upload the files.")
+print ("It will export the system specs to a folder on the c drive.")
+print ("are you sure you are having issues ONLY on ANCIENT?")
+quit=input("If you do, press Y or if you, dont press N to exit the utility")
+if quit=="N" or quit=="n":
+    sys.exit()
+else:
+    username=os.getlogin()
+    os.system('cmd /C "MSInfo32 /report C:\\frequencycs\\report.txt"')
+date=date.replace(" ","");date=date.replace("/","-");date=date.replace(":","-")
+os.rename("report.txt",username+"_"+"ANCIENT_"+date+".txt")
+
+user_file=os.listdir(path)[0] #OUTPUT NAME
+
+ssh=paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy)
+ssh.connect(hostname="64.227.38.17",username="server",password="Serverpassword1",port=22)
+
+sftp_client=ssh.open_sftp()
+sftp_client.put("C:/frequencycs/"+user_file,"/ancient/"+user_file)
+sftp_client.close()
+ssh.close()
+print("File uploaded")
+a = input("For FPS optimizations DM me on twitter! Press any key to exit")
+if a:
+    webbrowser.open_new_tab("https://twitter.com/fREQUENCYCS")
+    exit(0)
